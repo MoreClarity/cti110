5ac9656a79cc2e576aa9a218958617c886281ccd
MoreClarity <60890481+MoreClarity@users.noreply.github.com>
2020-02-12 12:02:23 -0500

    Add files via upload

---
+++ P2HW2_TurtleGraphic_HodgsonRyan.py
@@ -0,0 +1,37 @@
+#Drawing cardinal directions
+#02/12/2020
+#CTI-110 P2HW2-Turtle Graphic
+#Ryan Hodgson
+
+
+import turtle
+turtle.showturtle()
+def main():
+    turtle.forward(100)
+    turtle.write('east')
+    turtle.right(180)
+    turtle.forward(200)
+    turtle.write('west')
+    turtle.right(180)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.write('north')
+    turtle.right(180)
+    turtle.forward(200)
+    turtle.write('south')
+    turtle.right(180)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.penup
+    turtle.forward(15)
+    turtle.left(90)
+    turtle.pendown
+    turtle.circle(15)
+    turtle.hideturtle()
+
+main()
+
+
+
+
